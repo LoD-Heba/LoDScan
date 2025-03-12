458d32012325845967f942ca24451a8aa265cb7d
LoD-Heba <zegion511@gmail.com>
2025-03-11 23:11:39 -0600

    nada

---
+++ src/data/db.js
@@ -0,0 +1,98 @@
+const recentsNovel = [
+    {
+      id: 1,
+      title: "Stealing Spree",
+      time: "Hace 2 horas",
+      image: "src/assets/img/img6.jpg",
+    },
+    {
+      id: 2,
+      title: "Stealing Spree",
+      time: "Hace 2 horas",
+      image: "src/assets/img/img5.png",
+    },
+    {
+      id: 3,
+      title: "Stealing Spree",
+      time: "Hace 2 horas",
+      image: "src/assets/img/img4.jpg",
+    },
+    {
+      id: 4,
+      title: "Stealing Spree",
+      time: "Hace 2 horas",
+      image: "src/assets/img/img3.jpg",
+    },
+    {
+      id: 1,
+      title: "Stealing Spree",
+      time: "Hace 2 horas",
+      image: "src/assets/img/img6.jpg",
+    },
+    {
+      id: 2,
+      title: "Stealing Spree",
+      time: "Hace 2 horas",
+      image: "src/assets/img/img5.png",
+    },
+    {
+      id: 3,
+      title: "Stealing Spree",
+      time: "Hace 2 horas",
+      image: "src/assets/img/img4.jpg",
+    },
+    {
+      id: 4,
+      title: "Stealing Spree",
+      time: "Hace 2 horas",
+      image: "src/assets/img/img3.jpg",
+    },
+    {
+      id: 1,
+      title: "Stealing Spree",
+      time: "Hace 2 horas",
+      image: "src/assets/img/img6.jpg",
+    },
+    {
+      id: 2,
+      title: "Stealing Spree",
+      time: "Hace 2 horas",
+      image: "src/assets/img/img5.png",
+    },
+    {
+      id: 3,
+      title: "Stealing Spree",
+      time: "Hace 2 horas",
+      image: "src/assets/img/img4.jpg",
+    },
+    {
+      id: 4,
+      title: "Stealing Spree",
+      time: "Hace 2 horas",
+      image: "src/assets/img/img3.jpg",
+    },
+    {
+      id: 1,
+      title: "Stealing Spree",
+      time: "Hace 2 horas",
+      image: "src/assets/img/img6.jpg",
+    },
+    {
+      id: 2,
+      title: "Stealing Spree",
+      time: "Hace 2 horas",
+      image: "src/assets/img/img5.png",
+    },
+    {
+      id: 3,
+      title: "Stealing Spree",
+      time: "Hace 2 horas",
+      image: "src/assets/img/img4.jpg",
+    },
+    {
+      id: 4,
+      title: "Stealing Spree",
+      time: "Hace 2 horas",
+      image: "src/assets/img/img3.jpg",
+    },
+  ];
